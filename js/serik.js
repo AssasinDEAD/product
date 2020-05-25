c7fab58f2d1c33343fb8d3f4b54896aa44277b09
AssasinDEAD <64725895+AssasinDEAD@users.noreply.github.com>
2020-05-26 00:24:19 +0600

    I added adaptive

--- js/serik.js
+++ js/serik.js
@@ -1,39 +1,7 @@
 $(document).ready(function() {
-	// body...
-
-
-	$('.owl-carousel').owlCarousel({
-	    loop: true,
-	    margin:30,
-	    nav: true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        600:{
-	            items:1
-	        },
-	        1000:{
-	            items:2
-	        }
-	    }
-	});
-
-	$('.slide__item').owlCarousel({
-	    loop:true,
-	    margin: false,
-	    nav:true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        600:{
-	            items:1
-	        },
-	        1000:{
-	            items:2
-	        }
-	    }
+	$('.burger').click(function(event){
+		$('.burger,.menu__list').toggleClass('active')
+		$('body').toggleClass('lock')
 	});
 });
 
